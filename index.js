26b382e10a8ad68562aad09cb8f6161188de00db
Daniel <dandesantis14@gmail.com>
2021-10-17 09:01:41 -0400

    commit tree 79f3fafc0b5c70605dcae681a9e184ee171fe4a0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const findMatching = (array,name) => {
+    const match = array.filter(element => element.toLowerCase() === name.toLowerCase());
+    return match;
+}
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+// console.log(findMatching(drivers, 'bobby'));
+
+const fuzzyMatch = (array, letters) => {
+    const lettersLength = letters.length;
+    const match = array.filter(element => {
+        return element.slice(0,lettersLength).toLowerCase() === letters.toLowerCase()
+    });
+    return match;
+}
+
+const matchName = (drivers,name) => {
+    const match = drivers.filter(element => element.name === name);
+    return match;
+}
